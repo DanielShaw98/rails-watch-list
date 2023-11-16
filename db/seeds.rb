28cb6b1b2a0a8bf0d13a979d019287f1a496001c
Daniel Shaw <danielshaw71@gmail.com>
2023-11-16 16:42:09 +0000

    Completed challenge rails-watch-list

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'json'
+require 'open-uri'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?'
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
+user['results'].each do |movie|
+  Movie.create(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
